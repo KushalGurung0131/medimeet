64dc1d06198e3d02e3a848ea73e3cff710485f5f
KushalGurung0131 <grgkushal79@gmail.com>
2025-06-30 12:30:49 +0545

    05: Custom hooks and onboarding UI

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Pricing from "@/components/pricing";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -112,6 +113,7 @@ export default function Home() {
           </div>
           <div>
             {/* TODO: Pricing Table */}
+            <Pricing />
 
             <Card className="mt-12 bg-muted/20 border-emerald-900/30">
               <CardHeader>
